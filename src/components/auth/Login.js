96082bc62989a29a2a83fe73b617871c6cc61ddc
Lorin Jones <Lorin.Jones@gmail.com>
2022-11-10 22:00:54 -0500

    fixed login

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,15 +1,18 @@
 import React, { useState } from "react"
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom"
+import Button from 'react-bootstrap/Button';
+import backgroundVideo from "/home/walter/workspace/reaper/src/assets/VCRBlueScreen.mp4"
 import "./Login.css"
-
+ 
+ 
 export const Login = () => {
     const [email, set] = useState("lorin.jones@gmail.com")
     const navigate = useNavigate()
-
+ 
     const handleLogin = (e) => {
         e.preventDefault()
-
+ 
         return fetch(`http://localhost:8088/users?email=${email}`)
             .then(res => res.json())
             .then(foundUsers => {
@@ -19,41 +22,49 @@ export const Login = () => {
                         id: user.id,
                         fullName: user.fullName
                     }))
-
-                    navigate("/")
+ 
+                    navigate("/profile")
                 }
                 else {
                     window.alert("Invalid login")
                 }
             })
     }
-
+ 
     return (
         <main className="container--login">
-            <section>
-                <form className="form--login" onSubmit={handleLogin}>
-                    <h1>Reaper</h1>
-                    <h2>Please sign in</h2>
-                    <fieldset>
-                        <label htmlFor="inputEmail"> Email address </label>
-                        <input type="email"
-                            value={email}
-                            onChange={evt => set(evt.target.value)}
-                            className="form-control"
-                            placeholder="Email address"
-                            required autoFocus />
-                    </fieldset>
-                    <fieldset>
-                        <button type="submit">
-                            Sign in
-                        </button>
-                    </fieldset>
-                </form>
-            </section>
-            <section className="link--register">
-                <Link to="/register">Not a member yet?</Link>
+            <section className="outer-container">
+                <div className="inner-container">
+                        <form className="video-overlay" onSubmit={handleLogin}>
+                            <img src="https://res.cloudinary.com/dlr2tm7qr/image/upload/v1664422067/ReaperLogoFinal_hjhseb.png" class="main" ></img>
+                            <fieldset class="fieldset1">
+                                <label htmlFor="inputEmail"></label>
+                                <input type="email"
+                                    value={email}
+                                    onChange={evt => set(evt.target.value)}
+                                    className="form-control1"
+                                    placeholder="Email address"
+                                    required autoFocus />
+                            </fieldset>
+                            <fieldset>
+                                <Button
+                                    className="button"
+                                    type="submit">
+                                    Sign in
+                                </Button>
+                            </fieldset>
+                            <section className="link--register">
+                                <Link to="/register">Register</Link>
+                            </section>
+                        </form>
+                    <video autoPlay loop muted id="video1">
+                        <source src={backgroundVideo} type="video/mp4" />
+                    </video>
+               
+            </div>
+ 
             </section>
         </main>
     )
 }
-// fresh hell
+       
